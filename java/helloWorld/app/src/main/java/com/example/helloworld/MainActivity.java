69cb24ce5f6fad112c7668bdb2b6a9e9f7bc4ac4
matheus lima <matheusmt31@gmail.com>
2022-02-02 13:56:53 -0300

    override lifecycle methods only to see the result

--- java/helloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ java/helloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -13,8 +14,41 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding viewBinding;
     private int counter = 0;
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LIFECYCLE", "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LIFECYCLE", "onResume()");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("LIFECYCLE", "onPause()");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("LIFECYCLE", "onStop()");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("LIFECYCLE", "onDestroy()");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("LIFECYCLE", "onRestart()");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("LIFECYCLE", "onCreate()");
         viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = viewBinding.getRoot();
         setContentView(view);
