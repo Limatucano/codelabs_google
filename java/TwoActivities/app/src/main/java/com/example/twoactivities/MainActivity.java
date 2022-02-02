cbdf36c05e42b3f90f570c6f91741e1690e5b0bc
matheus lima <matheusmt31@gmail.com>
2022-02-02 20:19:51 -0300

    lifecycle in twoactivities

--- java/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ java/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.twoactivities.databinding.ActivityMainBinding;
@@ -12,9 +13,48 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.twoactivities.extra.MESSAGE";
     public static final int TEXT_REQUEST = 1;
     public ActivityMainBinding viewBinding;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "onCreate");
         viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
 
--- java/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ java/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -10,9 +10,48 @@ import com.example.twoactivities.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY = "com.example.twoactivities.extra.REPLY";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "onCreate");
         ActivitySecondBinding viewBinding = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
         Intent intent = getIntent();
