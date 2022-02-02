564a300b052188b60db2937c98866144abc92b87
matheus lima <matheusmt31@gmail.com>
2022-02-02 15:56:10 -0300

    hellotoast finished

--- java/HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ java/HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.hellotoast.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int mCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(viewBinding.getRoot());
+
+        viewBinding.countButton.setOnClickListener(onClick -> {
+            countUp();
+            updateResult(viewBinding);
+        });
+
+        viewBinding.toastButton.setOnClickListener(onClick -> {
+            showToast();
+        });
+    }
+
+    private void showToast(){
+        Toast.makeText(this,R.string.toast_message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void countUp(){
+        mCount++;
+    }
+    private void updateResult(ActivityMainBinding viewBinding){
+        viewBinding.resultText.setText(Integer.toString(mCount));
     }
 }
