19f655c8ec972a0faed413151082d5c6c245c5ae
matheus lima <matheusmt31@gmail.com>
2022-02-03 12:51:04 -0300

    implicit Intents: open Location and open web site

---
+++ java/ImplicitIntents/app/src/main/java/com/example/implictintent/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.implictintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.implictintent.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding viewBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(viewBinding.getRoot());
+
+        viewBinding.buttonOpenWebsite.setOnClickListener(onClick -> {
+            openWebSite();
+        });
+
+        viewBinding.buttonOpenLocation.setOnClickListener(onClick -> {
+            openLocation();
+        });
+
+        viewBinding.buttonShareText.setOnClickListener(onClick -> {
+
+        });
+    }
+
+    private void openLocation(){
+        String locationAddress = viewBinding.editOpenLocation.getText().toString();
+        Uri addessUri = Uri.parse("geo:0,0?q=" + locationAddress);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addessUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+    private void openWebSite(){
+        String url = viewBinding.editOpenWebsite.getText().toString();
+        Uri webPage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webPage);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+            return;
+        }
+        Log.d("ImplicitIntents", "Can't handle this!");
+    }
+}
