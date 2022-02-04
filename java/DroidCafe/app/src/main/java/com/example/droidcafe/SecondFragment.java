67788b3a1cb000c33ea2a5a5fc9f09871bb55aa6
matheus lima <matheusmt31@gmail.com>
2022-02-04 19:58:40 -0300

    add radio button and text inputs

--- java/DroidCafe/app/src/main/java/com/example/droidcafe/SecondFragment.java
+++ java/DroidCafe/app/src/main/java/com/example/droidcafe/SecondFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.droidcafe.databinding.FragmentSecondBinding;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SecondFragment extends Fragment {
 
@@ -40,8 +45,29 @@ public class SecondFragment extends Fragment {
 
         binding.textviewSecond.setText(this.messageReceived);
 
+        binding.nextday.setOnClickListener(this::onRadioButtonClicked);
+        binding.pickup.setOnClickListener(this::onRadioButtonClicked);
+
+        binding.sameday.setOnClickListener(this::onRadioButtonClicked);
+
     }
 
+    public void onRadioButtonClicked(View view){
+        Map<Integer,String> map = new HashMap(){{
+            put(R.id.sameday, getString(R.string.same_day_radio));
+            put(R.id.nextday, getString(R.string.next_day_radio));
+            put(R.id.pickup, getString(R.string.pick_up_radio));
+        }};
+
+        boolean isChecked = ((RadioButton) view).isChecked();
+        if(isChecked){
+            displayToast(map.get(view.getId()));
+        }
+
+    }
+    public void displayToast(String message) {
+        Toast.makeText(requireActivity(), message, Toast.LENGTH_SHORT).show();
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
