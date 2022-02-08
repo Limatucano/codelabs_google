74658cc9e047de8e617d227e9827321781e24e99
matheus lima <matheusmt31@gmail.com>
2022-02-08 15:11:02 -0300

    added business logic in viewModel and view logic in activity

--- java/ScoreKepper/app/src/main/java/com/example/scorekepper/MainActivity.java
+++ java/ScoreKepper/app/src/main/java/com/example/scorekepper/MainActivity.java
@@ -1,15 +1,52 @@
 package com.example.scorekepper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.scorekepper.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        binding.decreaseTeam1.setOnClickListener(view ->{
+            viewModel.decreaseScore(view).observe(this, score -> {
+                updateScore(binding.score1, score.toString());
+            });
+        });
+
+        binding.decreaseTeam2.setOnClickListener(view ->{
+           viewModel.decreaseScore(view).observe(this, score -> {
+               updateScore(binding.score2, score.toString());
+           });
+        });
+
+        binding.increaseTeam1.setOnClickListener(view -> {
+           viewModel.increaseScore(view).observe(this, score -> {
+               updateScore(binding.score1, score.toString());
+           });
+        });
+
+        binding.increaseTeam2.setOnClickListener(view ->{
+           viewModel.increaseScore(view).observe(this, score -> {
+               updateScore(binding.score2, score.toString());
+           });
+        });
+    }
+
+    private void updateScore(TextView view, String score){
+        view.setText(score);
     }
 }
---
+++ java/ScoreKepper/app/src/main/java/com/example/scorekepper/MainViewModel.java
@@ -0,0 +1,37 @@
+package com.example.scorekepper;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    private int mScore1 = 0;
+    private int mScore2 = 0;
+
+    public MutableLiveData decreaseScore(@NonNull View view){
+        switch(view.getId()){
+            case R.id.decreaseTeam1:
+                mScore1 -= 1;
+                return new MutableLiveData(mScore1);
+            case R.id.decreaseTeam2:
+                mScore2 -= 1;
+                return new MutableLiveData(mScore2);
+        }
+        return new MutableLiveData();
+    }
+
+    public MutableLiveData increaseScore(@NonNull View view){
+        switch(view.getId()){
+            case R.id.increaseTeam1:
+                mScore1 += 1;
+                return new MutableLiveData(mScore1);
+            case R.id.increaseTeam2:
+                mScore2 += 1;
+                return new MutableLiveData(mScore2);
+        }
+        return new MutableLiveData();
+    }
+}
