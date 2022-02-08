3a79177a346afcac479e4a01463b92ecab712f2b
matheus lima <matheusmt31@gmail.com>
2022-02-08 18:45:39 -0300

    added onMove in item

--- java/MaterialMe-Starter/app/src/main/java/com/example/android/materialme/MainActivity.java
+++ java/MaterialMe-Starter/app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -24,6 +24,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /***
  * Main Activity for the Material Me app, a mock sports news application
@@ -57,11 +58,15 @@ public class MainActivity extends AppCompatActivity {
         // Get the data.
         initializeData();
 
-        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT){
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT){
 
             @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder viewHolder1) {
-                return false;
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+                Collections.swap(mSportsData,from, to);
+                mAdapter.notifyItemMoved(from, to);
+                return true;
             }
 
             @Override
@@ -70,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
             }
         });
+
         helper.attachToRecyclerView(mRecyclerView);
     }
 
