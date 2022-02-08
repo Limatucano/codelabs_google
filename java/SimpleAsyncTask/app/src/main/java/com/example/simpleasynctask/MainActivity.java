0fec2826c27982d27ddb249c24a9e493c5fa27b8
matheus lima <matheusmt31@gmail.com>
2022-02-08 20:26:12 -0300

    simple async task

---
+++ java/SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.simpleasynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.simpleasynctask.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
