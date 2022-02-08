b97a2ecac2bdc22e2ee58b7625e535212f4c1f88
matheus lima <matheusmt31@gmail.com>
2022-02-07 21:37:56 -0300

    added feat to reset recyclerView

--- java/RecyclerView/app/src/main/java/com/example/recyclerview/FirstFragment.java
+++ java/RecyclerView/app/src/main/java/com/example/recyclerview/FirstFragment.java
@@ -1,13 +1,18 @@
 package com.example.recyclerview;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.recyclerview.databinding.FragmentFirstBinding;
@@ -18,7 +23,7 @@ public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
     private WordListAdapter mAdapter;
-    private final LinkedList<String> mWordList = new LinkedList<>();
+    private MutableLiveData<LinkedList<String>> mWordListMutable = new MutableLiveData<LinkedList<String>>();
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -30,16 +35,58 @@ public class FirstFragment extends Fragment {
 
     }
 
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        generateList();
+
+        this.mWordListMutable.observe(requireActivity(), new Observer<LinkedList<String>>() {
+            @Override
+            public void onChanged(LinkedList<String> mWords) {
+                Log.d("TESTE", mWords.toString());
+                setValuesToRecyclerView(mWords);
+            }
+        });
+
+
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        super.onCreateOptionsMenu(menu, inflater);
+        getActivity().getMenuInflater().inflate(R.menu.menu_main, menu);
+
+    }
+    public void generateList(){
+        LinkedList<String> mWordList = new LinkedList<>();
         for (int i = 0; i<20; i++){
             mWordList.addLast("Word" + i);
         }
-
+        this.mWordListMutable.postValue(mWordList);
+    }
+    public void setValuesToRecyclerView(LinkedList<String> mWordList){
         mAdapter = new WordListAdapter(requireContext(), mWordList);
         binding.recyclerview.setAdapter(mAdapter);
         binding.recyclerview.setLayoutManager(new LinearLayoutManager(requireContext()));
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        int id = item.getItemId();
+
+        if (id == R.id.action_reset) {
+            generateList();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
