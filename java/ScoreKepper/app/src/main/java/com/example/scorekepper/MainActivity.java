651f9c88838c40ee45341d36f76a8883efd282ab
matheus lima <matheusmt31@gmail.com>
2022-02-08 16:36:12 -0300

    created menu and changing theme

--- java/ScoreKepper/app/src/main/java/com/example/scorekepper/MainActivity.java
+++ java/ScoreKepper/app/src/main/java/com/example/scorekepper/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.scorekepper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,12 +16,19 @@ import com.example.scorekepper.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        if (savedInstanceState != null) {
+            updateScore(binding.score1, savedInstanceState.getString(STATE_SCORE_1));
+            updateScore(binding.score2, savedInstanceState.getString(STATE_SCORE_2));
+        }
 
         MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         binding.decreaseTeam1.setOnClickListener(view ->{
@@ -44,6 +54,32 @@ public class MainActivity extends AppCompatActivity {
                updateScore(binding.score2, score.toString());
            });
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the scores.
+        outState.putString(STATE_SCORE_1, binding.score1.getText().toString());
+        outState.putString(STATE_SCORE_2, binding.score2.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.night_mode){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        recreate();
+        return true;
     }
 
     private void updateScore(TextView view, String score){
