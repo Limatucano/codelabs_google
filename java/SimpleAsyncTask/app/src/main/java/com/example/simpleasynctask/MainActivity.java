7d09d693806f9a8faeab75f041c1fe71c86308ce
matheus lima <matheusmt31@gmail.com>
2022-02-08 20:57:49 -0300

    async task

--- java/SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
+++ java/SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.simpleasynctask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.simpleasynctask.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TEXT_STATE = "currentText";
     private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,22 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.button.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                new SimpleAsyncTask(binding.text).execute();
+            }
+        });
+        if(savedInstanceState != null){
+            binding.text.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, binding.text.getText().toString());
     }
 }
