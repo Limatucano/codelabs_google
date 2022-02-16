e5480378d22263f1639e02aa0338acb42a7395e3
matheus lima <matheusmt31@gmail.com>
2022-02-16 16:26:28 -0300

    appWidget adicionado textView com data de upload e button

--- java/AppWidgetSample/app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
+++ java/AppWidgetSample/app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
@@ -3,21 +3,34 @@ package com.example.appwidgetsample;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageItemInfo;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
 
+    private static final String mSharedPrefFile = "com.example.appwidgetsample";
+    private static final String COUNT_KEY = "count";
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        SharedPreferences prefs = context.getSharedPreferences(mSharedPrefFile,0);
+        int count = prefs.getInt(COUNT_KEY + appWidgetId,0);
+        count++;
+        String dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
+        views.setTextViewText(R.id.appWidgetId, String.valueOf(appWidgetId));
+        views.setTextViewText(R.id.appWidgetUpdate,context.getString(R.string.date_count_format, count, dateString));
+        SharedPreferences.Editor prefEditor = prefs.edit();
+        prefEditor.putInt(COUNT_KEY + appWidgetId, count);
+        prefEditor.apply();
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
@@ -30,13 +43,4 @@ public class NewAppWidget extends AppWidgetProvider {
         }
     }
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
 }
